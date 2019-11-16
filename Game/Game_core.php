2cae443a47ef47fbbeaff411dbbd8c3788d5ee93
JacekWy <34398043+JacekWy@users.noreply.github.com>
2019-11-16 19:53:02 +0100

    update

--- Game/Game_core.php
+++ Game/Game_core.php
@@ -8,20 +8,23 @@ $city_name = $conn->query("select City from data_game.Login where User ='$user'"
 $city_name_row = $city_name->fetch_assoc();
 $city_name_session = $city_name_row["City"];
 
-//Wood
-$wood = $conn->query("select wood from data_game.Login as login join data_game.user_data as mat on  login.id = mat.id where login.User = '$user'");
-$wood_row = $wood->fetch_assoc();
-$wood_row_session = $wood_row['wood'];
+//Show materials
+$material = $conn->query("select * from data_game.Login as login join data_game.user_data as mat on  login.id = mat.id where login.User = '$user'");
+$material_row = $material->fetch_assoc();
+$wood_row_session = $material_row['wood'];
+$stone_row_session = $material_row['stone'];
+$food_row_session = $material_row['food'];
 
-//stone
-$stone = $conn->query("select stone from data_game.Login as login join data_game.user_data as mat on  login.id = mat.id where login.User = '$user'");
-$stone_row = $stone->fetch_assoc();
-$stone_row_session = $stone_row['stone'];
 
-//food
-$food = $conn->query("select food from data_game.Login as login join data_game.user_data as mat on  login.id = mat.id where login.User = '$user'");
-$food_row = $food->fetch_assoc();
-$food_row_session = $food_row['food'];
+//Production Buildings
+$jsondata = file_get_contents("Material.json");
+$json = json_decode($jsondata,true);
+$json['Bulding']['Lvl'][1][0];
+
+//Production Wood
+
+
+
 
 
 
