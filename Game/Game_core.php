0a66d7a41deba2d54c8b36363514f428c4b5df8f
JacekWy <34398043+JacekWy@users.noreply.github.com>
2019-11-16 19:54:58 +0100

    al

--- Game/Game_core.php
+++ Game/Game_core.php
@@ -11,10 +11,6 @@ $city_name_session = $city_name_row["City"];
 //Show materials
 $material = $conn->query("select * from data_game.Login as login join data_game.user_data as mat on  login.id = mat.id where login.User = '$user'");
 $material_row = $material->fetch_assoc();
-$wood_row_session = $material_row['wood'];
-$stone_row_session = $material_row['stone'];
-$food_row_session = $material_row['food'];
-
 
 //Production Buildings
 $jsondata = file_get_contents("Material.json");
