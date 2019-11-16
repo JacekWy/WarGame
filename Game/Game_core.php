dd441ff41f8aaaad0497ea9c136db12f3ba66f4e
JacekWy <34398043+JacekWy@users.noreply.github.com>
2019-11-16 20:59:00 +0100

    set upd

--- Game/Game_core.php
+++ Game/Game_core.php
@@ -8,16 +8,21 @@ $city_name = $conn->query("select City from data_game.Login where User ='$user'"
 $city_name_row = $city_name->fetch_assoc();
 $city_name_session = $city_name_row["City"];
 
-//Show materials
+//Show materials,lvl
 $material = $conn->query("select * from data_game.Login as login join data_game.user_data as mat on  login.id = mat.id where login.User = '$user'");
 $material_row = $material->fetch_assoc();
 
-//Production Buildings
+//Get info production
 $jsondata = file_get_contents("Material.json");
 $json = json_decode($jsondata,true);
-$json['Bulding']['Lvl'][1][0];
 
-//Production Wood
+//Set production
+$prod_wood = $json['Bulding']['Lvl'][$material_row['wood_lvl']][0];
+$prod_stone = $json['Bulding']['Lvl'][$material_row['stone_lvl']][1];
+$prod_food = $json['Bulding']['Lvl'][$material_row['food_lvl']][2];
+
+
+
 
 
 
