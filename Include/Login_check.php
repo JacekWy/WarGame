1eb1588c9c00976c1194e715c6bf66da8c7dfe24
JacekWy <34398043+JacekWy@users.noreply.github.com>
2019-11-16 16:48:38 +0100

    sess

--- Include/Login_check.php
+++ Include/Login_check.php
@@ -7,12 +7,15 @@ $password = mysqli_real_escape_string ($conn , $_POST["Password"]);
 echo $user;
 echo $password;
 
+session_start();
+$_SESSION['user'] = $user;
+
 $login = $conn->query("select * from data_game.Login where User = '$user' and Password = '$password'");
 $row = $login->num_rows;
 
 if($row > 0){
     echo $row;
-
+    header('Location: ../Game/Game_show.html' );
 }else{
     $conn->close();
     header('Location: ../Index.html' );
